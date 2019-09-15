a6f2a1e71b2cc80f8f9825830ac12e99db602c60
risbo32 <ngoune32@gmail.com>
2019-09-15 10:25:51 +0100

    Ajout du fichier  readme

---
+++ SoftwareTas/SoftwareTas/Readme.md
@@ -0,0 +1,2 @@
+﻿Ceci est un fichier de Readme
+Le projet  a ete cree avec Visual studio 2015
---
+++ SoftwareTas/SoftwareTas/Readme.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SoftwareTas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Readme : ContentView
+    {
+        public Readme()
+        {
+            InitializeComponent();
+        }
+    }
+}
